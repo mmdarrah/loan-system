d7536f5da62ba979ce1fbc90951551eb8950d8fe
Hamade <mmdarrah@hotmail.com>
2020-09-16 16:10:03 +0200

    Format the code

--- classes/UsersControl.class.php
+++ classes/UsersControl.class.php
@@ -1,29 +1,34 @@
 <?php
-class UsersControl extends Users {
+class UsersControl extends Users
+{
 
-   public function setNewType($newType){
-       $this->setType($newType);
-   }
-
-   
-   public function addNewItem($type,$desc){
-         // filtrera input, notera sanitize int
-    $type = filter_input(INPUT_POST, 'type', FILTER_SANITIZE_NUMBER_INT);
-    $desc = filter_input(INPUT_POST, 'desc', FILTER_SANITIZE_SPECIAL_CHARS); 
-    $this->addItem($type, $desc);
+    public function setNewType($newType)
+    {
+        $this->setType($newType);
     }
 
-    public function delItem($id){
+    public function addNewItem($type, $desc)
+    {
+        // filtrera input, notera sanitize int
+        $type = filter_input(INPUT_POST, 'type', FILTER_SANITIZE_NUMBER_INT);
+        $desc = filter_input(INPUT_POST, 'desc', FILTER_SANITIZE_SPECIAL_CHARS);
+        $this->addItem($type, $desc);
+    }
+
+    public function delItem($id)
+    {
         $id = filter_input(INPUT_GET, 'delItem', FILTER_SANITIZE_NUMBER_INT);
         $this->deleteItem($id);
     }
 
-    public function delType($id){
+    public function delType($id)
+    {
         $id = filter_input(INPUT_GET, 'delType', FILTER_SANITIZE_NUMBER_INT);
         $this->deleteType($id);
     }
 
-    public function setNewLoan($return, $item, $user){
+    public function setNewLoan($return, $item, $user)
+    {
         // filtrera input, float för datum fältet tillåter - och .
         $return = filter_input(INPUT_POST, 'returndate', FILTER_SANITIZE_NUMBER_FLOAT);
         $item = filter_input(INPUT_POST, 'item', FILTER_SANITIZE_NUMBER_INT);
@@ -31,15 +36,18 @@ class UsersControl extends Users {
         $this->setLoan($return, $item, $user);
     }
 
-    public function showLoan(){
+    public function showLoan()
+    {
         $this->getLoan();
     }
 
-    public function showUsersLoan(){
+    public function showUsersLoan()
+    {
         $this->getUsersLoan();
     }
 
-    public function returnItem($id){
+    public function returnItem($id)
+    {
         $id = filter_input(INPUT_GET, 'returnItem', FILTER_SANITIZE_NUMBER_INT);
         $this->returnUsersItem($id);
     }
--- classes/UsersView.class.php
+++ classes/UsersView.class.php
@@ -1,33 +1,38 @@
 <?php
-class UsersView extends Users {
+class UsersView extends Users
+{
 
-    public function showUsers () {
+    public function showUsers()
+    {
         $row = $this->getUsers();
         echo '<h3>The users are: </h3>';
-        foreach($row as $user){
-        //echo '<pre>'; print_r($row); echo '</pre>';
-        echo 'Username: '. $user['username'] . "<br>";
-        echo 'First name: '. $user['firstname'] . "<br>" .'Last name: '. $user['lastname'] . "<br>";
-        echo 'Birthdate: '. $user['birthdate']. "<br>"; 
-        echo "<br>"; 
+        foreach ($row as $user) {
+            //echo '<pre>'; print_r($row); echo '</pre>';
+            echo 'Username: ' . $user['username'] . "<br>";
+            echo 'First name: ' . $user['firstname'] . "<br>" . 'Last name: ' . $user['lastname'] . "<br>";
+            echo 'Birthdate: ' . $user['birthdate'] . "<br>";
+            echo "<br>";
         }
-        
+
     }
 
-    public function showTypes(){
+    public function showTypes()
+    {
         $this->getTypes();
     }
 
-
-    public function showCategories(){
+    public function showCategories()
+    {
         $this->getCategories();
     }
 
-    public function showListOfItems(){
+    public function showListOfItems()
+    {
         $this->getListOfItems();
     }
 
-    public function showLoanList(){
+    public function showLoanList()
+    {
         $this->getLoanList();
     }
 }
