e6f3487ceb5793ebfef6d6923426e69604fce34a
Hamade <mmdarrah@hotmail.com>
2020-09-16 20:34:44 +0200

    Code cleaning

--- includes/autoLoader.inc.php
+++ includes/autoLoader.inc.php
@@ -2,17 +2,17 @@
 // Autoload all the classes inside the classes folder
 spl_autoload_register('autoLoader');
 
-function autoLoader ($className) {
+function autoLoader($className)
+{
 
-    $path= "classes/";
-    $extension = ".class.php";
+    $path = "classes/"; // All the classes should be in the classes folder
+    $extension = ".class.php"; // All the classes should have the same extension
     $fullPath = $path . $className . $extension;
 
     // if the file name is not existed
-    if (!file_exists($fullPath))
-    {
+    if (!file_exists($fullPath)) {
         return false;
     }
-  
+
     include_once $fullPath;
 }
